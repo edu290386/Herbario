140b7a48a062d6efdce99bf55b3c7b6c81fc3869
edu290386 <eduardo_torres@outlook.com>
2026-01-31 16:40:41 -0500

    modified:   app-plantas-pro/index.html 	modified:   app-plantas-pro/src/vercel.json 	modified:   app-plantas-pro/vite.config.js

--- app-plantas-pro/vite.config.js
+++ app-plantas-pro/vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: { https: true }, // Activa el modo HTTPS
-  plugins: [react(), mkcert()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    // Si estás en tu PC (development), activa HTTPS. En Vercel, no.
+    server: mode === "development" ? { https: true } : {},
+    plugins: [
+      react(),
+      // Solo carga mkcert si estás en tu PC
+      mode === "development" ? mkcert() : null,
+    ].filter(Boolean),
+  };
+});
